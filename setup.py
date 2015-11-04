0eee3b3dd4346f248565f55eefd3ff91644dd20b
cdansereau <christiandansereau@gmail.com>
2015-11-04 14:33:09 -0500

    edit the http path for Proteus

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='Proteus',
       version='0.1',
       description='Biomarker extraction pipeline for neuro-imaging data',
-      url='http://github.com/simexp/predicad',
+      url='https://github.com/cdansereau/Proteus,
       author='Christian Dansereau',
       author_email='christiandansereau@gmail.com',
       license='MIT',
