51995fc9e37ecf9e92c77f046407cded5f7876b8
Christian Dansereau <cdansereau@lg-1r14-n04.guillimin.clumeq.ca>
2016-10-17 12:45:56 -0400

    implement l1 norm for the first stage

--- proteus/predic/stacked2stage.py
+++ proteus/predic/stacked2stage.py
@@ -1,24 +1,21 @@
-__author__ = ' iChristian Dansereau'
+__author__ = 'Christian Dansereau'
 
 import numpy as np
 from sklearn.cluster import KMeans
 from proteus.predic import clustering as cls
 from proteus.matrix import tseries as ts
 from proteus.predic import prediction
-from proteus.predic import subtypes
 from scipy.spatial.distance import pdist, squareform
 from sklearn.cluster import MeanShift
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn import preprocessing
 from sklearn.feature_selection import RFECV
 from sklearn.svm import SVC, LinearSVC
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression,LogisticRegressionCV,RidgeCV
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import LeaveOneOut, LeavePOut, StratifiedKFold
 from sklearn.metrics import classification_report,accuracy_score
 
-from nistats import glm as nsglm
-import statsmodels.stats.multitest as smm
 import multiprocessing
 import time
 
@@ -28,26 +25,44 @@ class StackedPrediction:
     '''
 
     def makeNewModel(self,x,y):
-        clf = LogisticRegression(C=1.,class_weight='balanced',penalty='l2')
+        clf = LogisticRegression(C=1.,class_weight='balanced',penalty='l1')
+	#clf = LogisticRegressionCV(Cs=10,class_weight='balanced',penalty='l1',solver='liblinear')
         #clf = LinearSVC(C=1.,class_weight='balanced')
-        param_grid = dict(C=(10**np.arange(0.,-1.5,-0.5)))
-        gridclf = GridSearchCV(clf, param_grid=param_grid, cv=StratifiedKFold(y,n_folds=4), n_jobs=-1,scoring='accuracy')
-        gridclf.fit(x,y)
-        return gridclf.best_estimator_
+        param_grid = dict(C=(10**np.arange(1.,-2.5,-.25)))
+	gridclf = GridSearchCV(clf, param_grid=param_grid, cv=StratifiedKFold(y,n_folds=4), n_jobs=-1,scoring='precision_weighted')
+        #gridclf = GridSearchCV(clf, param_grid=param_grid, cv=StratifiedKFold(y,n_folds=4), n_jobs=-1,scoring='precision')
+	#gridclf = GridSearchCV(clf, param_grid=param_grid, cv=StratifiedKFold(y,n_folds=4), n_jobs=-1,scoring='accuracy')
+	gridclf.fit(x,y)
+	#clf.fit(x,y)
+        return gridclf.best_estimator_, gridclf.best_score_
+	#return clf,clf.C_
 
-    def fitMulti(self,x,y):
-        self.clf_list = []
+    def fitMulti(self,x,y,skip_init=False):
+	if not skip_init:
+	    self.clf_list = []
+            self.scores_stage1 = []
+
         for ii in range(x.shape[1]):
-            self.clf_list.append(self.makeNewModel(x[:,ii,:], y))
+	    if not skip_init:
+	    	model,score = self.makeNewModel(x[:,ii,:],y)
+	    	self.scores_stage1.append(score)
+            	self.clf_list.append(model)
+	    else:
+	    	self.clf_list[ii].fit(x[:,ii,:],y)	
+
+	if not skip_init:
+	    print self.scores_stage1
 
 
     def predictMulti(self,x):
         pred = []
         for ii in range(len(self.clf_list)):
             if len(x.shape) == 2:
-                pred.append(self.clf_list[ii].predict(x[ii,:].reshape(1,-1)))
+                #pred.append(self.clf_list[ii].predict(x[ii,:].reshape(1,-1)))
+		pred.append(self.clf_list[ii].decision_function(x[ii,:].reshape(1,-1)))
             else:
-                pred.append(self.clf_list[ii].predict(x[:,ii,:]))
+                #pred.append(self.clf_list[ii].predict(x[:,ii,:]))
+		pred.append(self.clf_list[ii].decision_function(x[:,ii,:]))
        #     print self.clf_list[ii]
        #     print (self.clf_list[ii].coef_**2.).sum()
         pred = np.array(pred)
@@ -59,21 +74,23 @@ class StackedPrediction:
 
     def fit(self,x,y):
         # Stage 1
-        hm = self.estimate_hitmiss(x, y)
-        #self.fitMulti(x,y)
+        #hm = self.estimate_hitmiss(x, y)
+        self.fitMulti(x,y)
+	hm,ref_labels = self.predict_estimate(x,y)
+	print 'hm shape: ',hm.shape
         #hm = self.predictMulti(x)
 
         print (hm == np.tile(y,(hm.shape[1],1)).T).mean(axis=0)
         # Stage 2
-        clf2 = LogisticRegression(C=1.,class_weight='balanced',penalty='l2',max_iter=3)
-        param_grid = dict(C=(10**np.arange(0.,-.5,-0.25)))
+        clf2 = LogisticRegression(C=1.,class_weight='balanced',penalty='l2')
+        param_grid = dict(C=(10**np.arange(5.,-5.,-0.1)))
 
-        gridclf = GridSearchCV(clf2, param_grid=param_grid, cv=StratifiedKFold(y,n_folds=4), n_jobs=-1,scoring='accuracy')
-        gridclf.fit(hm,y)
+        gridclf = GridSearchCV(clf2, param_grid=param_grid, cv=StratifiedKFold(ref_labels,n_folds=4), n_jobs=-1,scoring='accuracy')
+        gridclf.fit(hm,ref_labels)
 
         self.clf_stack = gridclf.best_estimator_
-        print 'stack',self.clf_stack
-        print 'stack',(self.clf_stack.coef_**2.).sum()
+        print 'stack ',self.clf_stack
+        print 'stack SOS  ',(self.clf_stack.coef_**2.).sum()
 
     def cv(self,X,y,gs=4):
         k=1
@@ -100,6 +117,22 @@ class StackedPrediction:
         #y_pred1 = self.clf1.predict(xw)
         #y_pred2 = self.clf2.decision_function(xw)
         #return np.array([y_pred1,y_pred2]).T
+
+    def predict_estimate(self,x,y,kf=4):
+        # Perform a kfold CV to estimate the actual prediction error
+	skf = StratifiedKFold(y, n_folds=kf)
+        results = []
+        ref_labels = []
+	for train_index, test_index in skf:
+            self.fitMulti(x[train_index,:,:],y[train_index],skip_init=True)
+            results.append(self.predictMulti(x[test_index,:,:]))
+	    ref_labels.extend(y[test_index])
+        ref_labels = np.array(ref_labels)
+	results = np.vstack(results)
+
+        self.fitMulti(x,y,skip_init=True)
+        return results, ref_labels
+
 
     def estimate_hitmiss(self,x,y):
         # Perform a LOO to estimate the actual HM
